26a72180f31eab38f5cdd7e73eafd0b87f0974ee
alejocastaram <alejo.castaram@gmail.com>
2026-03-09 11:26:34 -0500

    initial commit

---
+++ serverles-guru-challenge/src/main/java/com/alejandrocastaneda/serverles_guru_challenge/ServerlesGuruChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alejandrocastaneda.serverles_guru_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerlesGuruChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerlesGuruChallengeApplication.class, args);
+	}
+
+}
---
+++ serverles-guru-challenge/src/test/java/com/alejandrocastaneda/serverles_guru_challenge/ServerlesGuruChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandrocastaneda.serverles_guru_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerlesGuruChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
