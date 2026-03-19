f76b4003a10ba9cd3b36bf8ac65ae55c8104afc2
alejocastaram <alejo.castaram@gmail.com>
2026-03-19 15:27:10 -0500

    refactor for lambda deployment

--- serverles-guru-challenge/src/main/java/com/alejandrocastaneda/serverles_guru_challenge/infrastructure/adapter/inbound/lambda/LambdaHandler.java
+++ serverles-guru-challenge/src/main/java/com/alejandrocastaneda/serverles_guru_challenge/infrastructure/adapter/inbound/lambda/LambdaHandler.java
@@ -1,11 +1,9 @@
 package com.alejandrocastaneda.serverles_guru_challenge.infrastructure.adapter.inbound.lambda;
 
-import com.alejandrocastaneda.serverles_guru_challenge.ServerlesGuruChallengeApplication;
 import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
 
 public class LambdaHandler extends FunctionInvoker {
     public LambdaHandler() {
-        // Pasa aquí tu clase principal que tiene el @SpringBootApplication
-        super("com.alejandrocastaneda.serverles_guru_challenge.ServerlesGuruChallengeApplication");
+        super();
     }
 }
