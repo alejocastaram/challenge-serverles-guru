0423ed5c3f79610f8b4029cf8fc5c040d257ab2f
alejocastaram <alejo.castaram@gmail.com>
2026-03-18 23:24:30 -0500

    Delete LambdaHandler.java

--- serverles-guru-challenge/src/main/java/com/alejandrocastaneda/serverles_guru_challenge/infrastructure/adapter/inbound/lambda/LambdaHandler.java
+++
@@ -1,6 +0,0 @@
-package com.alejandrocastaneda.serverles_guru_challenge.infrastructure.adapter.inbound.lambda;
-
-import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
-
-public class LambdaHandler extends FunctionInvoker {
-}
